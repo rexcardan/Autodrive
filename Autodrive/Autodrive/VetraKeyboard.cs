2d0f1edac5b0e7fc409cbca57682cba103b9df70
Rex Cardan <rexcardan@gmail.com>
2016-12-23 16:23:31 -0600

    Added files

---
+++ Autodrive/Autodrive/VetraKeyboard.cs
@@ -0,0 +1,36 @@
+﻿using Autodrive.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Autodrive
+{
+    public class VetraKeyboard : IKeyboard
+    {
+        private string comPort = "COM3";
+
+        private Dictionary<Key, string> asciiTable = new Dictionary<Key, string>()
+        {
+            {Key.A, "41" }
+        };
+
+        public bool PressKey(Key k)
+        {
+            if (asciiTable.ContainsKey(k))
+            {
+                var ascii = asciiTable[k];
+                SendMessage(ascii);
+                return true;
+            }
+            return false;
+        }
+
+        private void SendMessage(string ascii)
+        {
+            var serial = new Serial
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sp = new SerialPort("COM3");
+            sp.BaudRate = 9600;
+            sp.DataBits = 8;
+            sp.StopBits = StopBits.One;
+            sp.Open();
+
+            while (true)
+            {
+                var message = Console.ReadLine();
+                var key = Task.Run(() =>
+                {
+                    var keyPressed = Console.ReadKey();
+                    return keyPressed;
+                });
+                sp.WriteLine(message);
+                var k = key.Result;
+            }
+        }
+    }
+}
