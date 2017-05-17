196bb4db17cc57eced42dacbb563d87df41afc0c
rexcardan <rexcardan@gmail.com>
2017-05-17 16:26:04 -0500

    Updated splash screen to be more stable

--- Autodrive/ExcelRunner/App.xaml.cs
+++ Autodrive/ExcelRunner/App.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using Autodrive.Splash;
+using ExcelRunner.Views;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Navigation;
 
 namespace ExcelRunner
 {
@@ -15,13 +18,6 @@ namespace ExcelRunner
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            var splash = Splasher.GetSplash();
-            splash.Show();
-
-
-            base.OnStartup(e);
-        }
+       
     }
 }
